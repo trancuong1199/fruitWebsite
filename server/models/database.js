c67f6a58352bedafa70b5680bdec74df98810680
cuongtt <trancuong181299@gmail.com>
2023-08-14 22:02:44 +0700

    test

--- server/models/database.js
+++
@@ -1,9 +0,0 @@
-//Database trả về kết nối với cơ sở dữ liệu
-var mysql = require('mysql');
-var db = mysql.createConnection({
-   host: 'localhost', user: 'root', password: '', 
-   database: 'asm-reactjs'
-}); 
-db.connect(() => console.log('Da ket noi database !'));
-module.exports = db; 
-//lệnh exports để xuất (public) ra, cho ngoài module dùng được db
